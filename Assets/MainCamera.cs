fd5a1bc04b3012d487c8a7dbe41d9373480f647b
Brent Kim <devgrapher@gmail.com>
2016-11-06 17:34:03 +0900

    fix main camera Y coordinate

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+	Vector3 distanceToBear;
+
+	// Use this for initialization
+	void Start () {
+		distanceToBear = Bear.instance.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 new_pos = Bear.instance.transform.position - distanceToBear;
+		new_pos.y = transform.position.y;
+		transform.position = new_pos;
+	}
+}
