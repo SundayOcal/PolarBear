133e4869afef9d87ac7beba614f14e80dd21ef15
Brent Kim <devgrapher@gmail.com>
2017-04-01 15:35:04 +0900

    Fix BG music duplicateing

--- Assets/Script/BgSound.cs
+++ Assets/Script/BgSound.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class BgSound : MonoBehaviour {
+	public static BgSound instance;
 
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Awake () {
+		if (instance) {
+			Destroy (this.gameObject);
+		} else {
+			instance = this;
+		}
 	}
 	
 	// Update is called once per frame
